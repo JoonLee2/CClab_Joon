0762a7400c03a10e11815ec8f7a6243023debffe
Joon <gkrwns9804@gmail.com>
2021-11-14 21:20:49 +0900

    First website

---
+++ empty-project/empty-project/js/script.js
@@ -0,0 +1,39 @@
+console.log("Loaded!");
+
+function setup() {
+  createCanvas(400, 500);
+
+}
+function draw() {
+  background(100);
+
+}
+
+
+
+/*
+  let btn = document.createElement("button");
+  btn.style.width= "100px";
+  btn.style.height = "80px";
+  btn.style.margin = "10px";
+  btn.addEventListener("click", change );
+  document.body.appendChild(btn);
+
+  function change() {
+  alert("HEY!"");
+  //document.body.style.backgroundColor = "pink";
+}
+
+/*for ( let i = 0; i <100 ; i++ ) {
+  //
+  let elt = document.createElement("div");
+  elt.style.backgroundColor = "yellow"
+
+  elt.style.width= "100px";
+  elt.style.height = "80px";
+  elt.style.margin = "10px";
+  document.body.appendChild(elt);
+
+
+}
+*/
